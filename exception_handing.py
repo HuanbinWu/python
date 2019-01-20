a9be7f8c5eb9f0d18d6ca33727d649026adea7e2
HuanbinWu <huanbin2580@outlook.com>
2019-01-20 19:54:32 +0800

    first

---
+++ exception_handing.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# Author:HuanBing
+# i=j
+# print())
+# a='123'
+# # # print(a[3])
+# d={'a':1,'b':2}
+# print(d['3'])
+# try:
+#     year=int(input('input year:'))
+# except ValueError:
+# #     print('年份要输入数字')
+# a=123
+# a.append()
+# except {ValueError,AttributeError,KeyError}
+# try:
+# #     print(1/0)
+# # except Exception as e:
+# #     print('%s' %e)
+# # try:
+# #     raise  NameError('helleError')
+# # except NameError:
+# #     print('my custon error')
+try:
+    a=open('weapon.txt')
+    a.writelines('青龙偃月刀')
+except Exception as e:
+    print(e)
+finally:
+    a.close()
---
+++ test_dict.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# Author:HuanBing
+dict1={}
+print(type(dict1))
+dict2 = {'x':1,'y':2}
+dict2['z']=3
+
+print(dict2)
